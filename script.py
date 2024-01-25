8a86e3a3bc5d8de956aac4158c84563b339aec08
Daniel Borrego <154102665+dborrego909@users.noreply.github.com>
2024-01-24 21:52:54 -0800

    first run of python

---
+++ script.py
@@ -0,0 +1,41 @@
+  Python 3.12.1 (v3.12.1:2305ca5144, Dec  7 2023, 17:23:38) [Clang 13.0.0 (clang-1300.0.29.30)] on darwin
+Type "help", "copyright", "credits" or "license()" for more information.
+print('Hello world')
+Hello world
+
+'42' + 3
+Traceback (most recent call last):
+  File "<pyshell#2>", line 1, in <module>
+    '42' + 3
+TypeError: can only concatenate str (not "int") to str
+2 + 2
+4
+2
+2
+2 + 3 * 6
+20
+(2 + 3) *6
+30
+'Alice' +
+SyntaxError: invalid syntax
+'Alice' + 'bob'
+'Alicebob'
+'Alice' * 5
+'AliceAliceAliceAliceAlice'
+spam = 40
+spam
+40
+eggs = 2
+eggs
+2
+spam + eggs + spam
+82
+spam = spam + 2
+spam
+42
+spam = 'Hello'
+spam
+'Hello'
+spam = 'Goodbye'
+spam
+'Goodbye'
